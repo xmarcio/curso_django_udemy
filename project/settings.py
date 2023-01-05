8cd68625dfb376f5d58b368a5bee5669c74f7f99
Marcio Seefeld <marcioseefeld@gmail.com>
2023-01-05 11:00:32 -0300

    html create

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-03!r=@ks&&rn_b(9m84o9+_hn8e(5+976pawc82%w=xrp-ygzr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+
+    'recipes',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,9 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_template'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
