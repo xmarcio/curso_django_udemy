91cf6cabbb181cf430ff954e7240e406deba73b8
Marcio Seefeld <marcioseefeld@gmail.com>
2023-01-09 17:22:05 -0300

    update

--- recipes/models.py
+++ recipes/models.py
@@ -31,7 +31,8 @@ class Recipe(models.Model):
         upload_to='recipes/covers/%Y/%m/%d', blank=True, default='')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True, default=None)
 
     class Meta:
         verbose_name = 'Receita'
--- recipes/urls.py
+++ recipes/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = 'receitas'
+app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('recipes/category/<int:category_id>/', views.category, name="categoria"),
-    path('recipes/<int:id>/', views.recipe, name="receita"),
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
